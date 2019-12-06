c27c98209fcc8271a94210945e7b44afe174a6a5
Luke Lai <luke@lukeelai.com>
2019-12-06 09:52:21 -0800

    get challenger players

--- src/containers/HomePageContainer.js
+++ src/containers/HomePageContainer.js
@@ -10,16 +10,22 @@ import HomePage from "../components/HomePage";
 
 class HomePageContainer extends React.Component {
   componentDidMount = () => {
-    console.log("Home Page Container");
     this.getSummoner();
   };
 
   getSummoner = () => {
     const ranked = "RANKED_SOLO_5x5";
     const uri = `/lol/league/v4/challengerleagues/by-queue/${ranked}/?api_key=${process.env.REACT_APP_LEAGUE_API_KEY}`;
+    const summonerUri = ``;
     axios(uri)
       .then(result => {
-        console.log(result.data);
+        const newData = result.data.entries.map(summoner => {
+          return {
+            summonerName: summoner.summonerName,
+            summonerId: summoner.summonerId
+          };
+        });
+        console.log(newData);
         this.props.addSummoner(result.data.entries);
       })
       .catch(e => {
