bdd6e24373abd14ab17f26d55b7e021b804e4d13
Luke Lai <luke@lukeelai.com>
2019-12-10 07:36:26 -0800

    setup proxy  and firebase

--- src/containers/HomePageContainer.js
+++ src/containers/HomePageContainer.js
@@ -8,28 +8,51 @@ import { addSummoner } from "../actions/summoner";
 //Components
 import HomePage from "../components/HomePage";
 
+//DB
+import firebase from "../firebase/firebase";
+
 class HomePageContainer extends React.Component {
-  componentDidMount = () => {
+  componentWillMount = () => {
     this.getSummoner();
   };
 
   getSummoner = () => {
     const ranked = "RANKED_SOLO_5x5";
     const uri = `/lol/league/v4/challengerleagues/by-queue/${ranked}/?api_key=${process.env.REACT_APP_LEAGUE_API_KEY}`;
-    const summonerUri = ``;
     axios(uri)
       .then(result => {
         const newData = result.data.entries.map(summoner => {
+          const db = firebase.firestore();
+          db.settings({ timestampsInSnapshots: true });
+          let setDoc = db
+            .collection("challenger")
+            .doc(summoner.summonerName)
+            .set({
+              summonerName: summoner.summonerName,
+              summonerId: summoner.summonerId,
+              accountId: ""
+            });
           return {
             summonerName: summoner.summonerName,
-            summonerId: summoner.summonerId
+            summonerId: summoner.summonerId,
+            accountId: ""
           };
         });
-        console.log(newData);
-        this.props.addSummoner(result.data.entries);
+        this.props.addSummoner(newData);
       })
       .catch(e => {
         console.log(e);
+      });
+  };
+
+  getAccountId = summonerId => {
+    const summonerUri = `/lol/summoner/v4/summoners/${summonerId}/?api_key=${process.env.REACT_APP_LEAGUE_API_KEY}`;
+    axios(summonerUri)
+      .then(result => {
+        console.log(result.data);
+      })
+      .catch(e => {
+        console.log("summoner", e);
       });
   };
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,4 +4,9 @@ module.exports = app => {
     "/lol/league/v4/challengerleagues/by-queue/",
     proxy({ target: "https://na1.api.riotgames.com", changeOrigin: true })
   );
+
+  app.use(
+    "/lol/summoner/v4/summoners/",
+    proxy({ target: "https://na1.api.riotgames.com", changeOrigin: true })
+  );
 };
